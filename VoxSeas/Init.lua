d918fb630706639c29e236a3c5c3575a9e256bb8
Levi Adler <levialbuquerque05@gmail.com>
2025-07-29 14:40:15 -0300

    Correção do nome da pasta

--- VoxSeas/Init.lua
+++ VoxSeas/Init.lua
@@ -1,4 +1,4 @@
-local base = "https://raw.githubusercontent.com/LeviAdler05/Testando-Scripts/main/"
+local base = "https://raw.githubusercontent.com/LeviAdler05/Roblox-Scripts/main/VoxSeas/"
 loadstring(game:HttpGet(base .. "Source.lua"))()
 loadstring(game:HttpGet(base .. "Utils.lua"))()
 loadstring(game:HttpGet(base .. "TP.lua"))()
